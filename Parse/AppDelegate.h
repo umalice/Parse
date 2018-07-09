d597cb8245934a7fa1b03548e6dc3d943c14ffce
Alice Park <alicep@fb.com>
2018-07-09 10:16:54 -0700

    Initial Commit

---
+++ Parse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Parse
+//
+//  Created by Alice Park on 7/9/18.
+//  Copyright © 2018 Alice Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
